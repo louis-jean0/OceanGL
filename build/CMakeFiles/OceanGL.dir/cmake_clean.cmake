e9190f9b9a69e390f401c8af29c9ce678338a9c3
louis <louis.jean34@hotmail.fr>
2024-02-19 17:39:13 +0100

    Setup GLFW & glad

---
+++ build/CMakeFiles/OceanGL.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OceanGL.dir/src/OceanGL.cpp.o"
+  "CMakeFiles/OceanGL.dir/src/OceanGL.cpp.o.d"
+  "OceanGL"
+  "OceanGL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OceanGL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/external/glfw-3.3.9/tests/CMakeFiles/triangle-vulkan.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/louis/Documents/TER-OceanGL/external/glfw-3.3.9/deps/glad_vulkan.c" "external/glfw-3.3.9/tests/CMakeFiles/triangle-vulkan.dir/__/deps/glad_vulkan.c.o" "gcc" "external/glfw-3.3.9/tests/CMakeFiles/triangle-vulkan.dir/__/deps/glad_vulkan.c.o.d"
+  "/home/louis/Documents/TER-OceanGL/external/glfw-3.3.9/tests/triangle-vulkan.c" "external/glfw-3.3.9/tests/CMakeFiles/triangle-vulkan.dir/triangle-vulkan.c.o" "gcc" "external/glfw-3.3.9/tests/CMakeFiles/triangle-vulkan.dir/triangle-vulkan.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/louis/Documents/TER-OceanGL/build/external/glfw-3.3.9/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
