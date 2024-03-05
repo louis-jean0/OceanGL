7c7e0e6830a5a21f2b9d0210fcaebeec9a700af5
e20190010736 <louis.jean34@hotmail.fr>
2024-03-05 16:59:40 +0100

    Mise au propre base de code

--- src/OceanGL.cpp
+++ src/OceanGL.cpp
@@ -1,51 +1,121 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+#include "Window.h"
 #include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+#include <stdbool.h>
+#include <unistd.h>
+#include <sys/sysinfo.h>
+#include <thread>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-        glViewport(0, 0, width, height);
-}
-
-void processInput(GLFWwindow *window)
-{
-    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
+long long GetMemoryUsage() {
+    long long memory_usage = 0;
+    std::ifstream status_file("/proc/self/status");
+    if (status_file.is_open()) {
+        std::string line;
+        while (std::getline(status_file, line)) {
+            if (line.find("VmRSS:") != std::string::npos) {
+                std::istringstream iss(line);
+                std::string label;
+                long long value;
+                iss >> label >> value;
+                memory_usage = value;
+                break;
+            }
+        }
+        status_file.close();
+    }
+    return memory_usage;
 }
 
 int main() {
 
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    Window window(4, 6, 1280, 720, "TER - OceanGL", true);
+    window.setup_GLFW();
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
-    if (window == NULL)
-    {
-        std::cout<<"Failed to create GLFW window"<<std::endl;
-        glfwTerminate();
-        return -1;
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(window.get_window(), true);
+    ImGui_ImplOpenGL3_Init("#version 460");
+
+
+    while (!glfwWindowShouldClose(window.get_window())) {
+        glfwPollEvents();   
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        float fps = ImGui::GetIO().Framerate;
+        float ms_per_frame = 1000.0f / fps;
+
+        ImGui::Begin("Paramètres");
+
+        ImGui::Separator();
+        ImGui::Spacing();
+
+        ImGui::Text("Métriques de l'ordinateur (Debug) :");
+        ImGui::Spacing();
+        ImGui::Spacing();
+        ImGui::Spacing();
+
+        char hostname[256];
+        gethostname(hostname, sizeof(hostname));
+        ImGui::Text("Nom de l'ordinateur: %s", hostname);
+
+        FILE *fp = popen("uname -sr", "r");
+        char os[256];
+        fgets(os, sizeof(os), fp);
+        pclose(fp);
+        ImGui::Text("Système d'exploitation: %s", os);
+
+        struct sysinfo si;
+        sysinfo(&si);
+        double ram_totale = si.totalram * si.mem_unit / (1024 * 1024 * 1024); // Convertir en Go
+        ImGui::Text("Mémoire RAM totale : %.2f Go", ram_totale);
+
+        long long ram_utilisee_programme = GetMemoryUsage();
+        double ram_utilisee_go = static_cast<double>(ram_utilisee_programme) / (1024 * 1024); // Convertir en Mo
+        double pourcentage_ram_utilisee = (ram_utilisee_go / ram_totale) * 100.0; // Calculer le pourcentage
+        ImGui::Text("Mémoire RAM utilisée par le programme : %.4f Mo", ram_utilisee_go);
+        ImGui::Text("Mémoire RAM utilisée par le programme (en %%) : %.4f%%", pourcentage_ram_utilisee);
+        ImGui::Text("FPS : %.2f / MS (Milliseconds Per Frame) : %.2f", fps, ms_per_frame);
+
+        ImGui::Spacing();
+        ImGui::Separator();
+
+        ImGui::Text("Autres");
+
+        ImGui::End();
+
+
+        int display_w, display_h;
+        glfwGetFramebufferSize(window.get_window(), &display_w, &display_h);
+        glViewport(0, 0, 1280, 720);
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData()); 
+
+        glfwSwapBuffers(window.get_window()); 
     }
-    glfwMakeContextCurrent(window);
 
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 
-    glViewport(0, 0, 800, 600);
-
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-    while(!glfwWindowShouldClose(window))
-    {
-        processInput(window);
-        glfwSwapBuffers(window);
-        glfwPollEvents();    
-    }
-
-    glfwTerminate();
+    window.~Window();
+    
     return 0;
 }
---
+++ src/Window.cpp
@@ -0,0 +1,76 @@
+#include "Window.h"
+
+Window::Window(unsigned int major, unsigned int minor, int width, int height, const char *title, bool Vsync) {
+    this->minor = minor;
+    this->major = major;
+    this->height = height;
+    this->width = width;
+    this->title = title;
+    this->Vsync = Vsync;
+}
+
+void Window::setup_GLFW() {
+    // Initialisation de GLFW
+    glfwInit();
+    if (!glfwInit()) {
+        exit(0);
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, this->major);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,  this->minor);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Création de la fenêtre
+    this->window = glfwCreateWindow(this->width, this->height, this->title, NULL, NULL);
+    if (window == NULL) {
+        std::cout<<"Failed to create GLFW window"<<std::endl;
+        glfwTerminate();
+        exit(1);
+    }
+
+    glfwMakeContextCurrent(this->window);
+
+    if(this->Vsync == true) {
+        glfwSwapInterval(1);
+    } else if (this->Vsync == false) {
+        glfwSwapInterval(0);
+    }
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        exit(2);
+    }
+
+    glViewport(0, 0, this->width, this->height);
+
+    glEnable(GL_DEPTH_TEST);
+}
+
+Window::~Window() {
+    glfwDestroyWindow(this->window);
+    glfwTerminate();
+}
+
+int Window::get_height() {
+    return this->height;
+}
+
+int Window::get_width() {
+    return this->width;
+}
+
+const char * Window::get_title() {
+    return this->title;
+}
+
+unsigned int Window::get_major() {
+    return this->major;
+}
+
+unsigned int Window::get_minor() {
+    return this->minor;
+}
+
+GLFWwindow* Window::get_window() {
+    return this->window;
+}
